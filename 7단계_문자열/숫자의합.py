f1b7328741aefca8ab05c7a8d4d519d87332a59f
wnstjd9701 <wnstjd9701@naver.com>
2021-05-12 13:09:03 +0900

    BAEKJOON

--- 7단계_문자열/숫자의합.py
+++ 7단계_문자열/숫자의합.py
@@ -1,6 +1,8 @@
 # 백준 11720번 문제 - 숫자의 합
-n = int(input())
-num = int(input())
-st = list(map(str, num))
-print(st)
+a = int(input())
+b = list(input())
+result = 0
 
+for i in b:
+    result += int(i)
+print(result)
