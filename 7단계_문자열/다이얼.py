f0611ba4d65ddac6541037c529ec2df92ea5c0c8
wnstjd9701 <wnstjd9701@naver.com>
2021-05-13 00:44:13 +0900

    BAEKJOON

--- 7단계_문자열/다이얼.py
+++ 7단계_문자열/다이얼.py
@@ -4,7 +4,7 @@ dial = ["abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
 time = 0
 for i in range(len(word)):
     for j in dial:
-        if word[i] in j:
+        if word[i] in j: # word[i] 가 j안에 있을 경우
             time += dial.index(j) + 3
 
 print(time)
