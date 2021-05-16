4975eb14f69ff2ab0719d824910ec3126ca4b2ed
wnstjd9701 <wnstjd9701@naver.com>
2021-05-16 18:57:51 +0900

    BAEKJOON

--- 8단계_기본수학1/손익분기점.py
+++ 8단계_기본수학1/손익분기점.py
@@ -1,5 +1,7 @@
 # 백준 1712번 문제 - 손익분기점
 A, B, C = map(int, input().split())
-count = 0 
-num = int(A / (C-B))
-print(num+1)
+if B>=C:
+    print(-1)
+else:
+    num = int(A / (C-B))
+    print(num+1)
