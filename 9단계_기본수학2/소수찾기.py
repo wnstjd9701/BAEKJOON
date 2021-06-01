a768771dfc0c62c7678da74efe0d1423810c058a
wnstjd9701 <wnstjd9701@naver.com>
2021-06-01 17:19:08 +0900

    BAEKJOON

---
+++ 9단계_기본수학2/소수찾기.py
@@ -0,0 +1,17 @@
+# 백준 1978번 문제 - 소수찾기
+n = int(input())
+sosu = list(map(int, input().split()))
+def prime(num):
+    if num == 1:
+        return False
+    elif num == 2:
+        return True
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+count = 0
+for i in sosu:
+    if prime(i):
+        count += 1
+print(count)
