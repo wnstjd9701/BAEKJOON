ed24a9d626355ca0234e2499f3cc03c04bb2e7a8
wnstjd9701 <wnstjd9701@naver.com>
2021-06-01 17:00:47 +0900

    Python

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-arr = []
-index = 0
-for i in range(2,10001):
-    arr.append(str(i))
-arr = list(map(int,arr))
-
-for i in range(2,101):
-    mul = 1
-    while i*mul <= 10000:
-        if i*mul in arr:
-            if i*mul>=2 and i*mul<=100:
-                arr
-            else:
-                arr.remove(i*mul)
-        mul+=1   
+# test.py
+arr_list = []
