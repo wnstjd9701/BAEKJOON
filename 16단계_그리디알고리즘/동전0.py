53cddaef4ccd22521728186a84b97f5f3cb5a056
wnstjd9701 <wnstjd9701@naver.com>
2021-06-15 17:17:40 +0900

    BAEKJOON

--- 16단계_그리디알고리즘/동전0.py
+++ 16단계_그리디알고리즘/동전0.py
@@ -2,16 +2,18 @@
 n, k = map(int, input().split())
 coin_list = []
 for i in range(n):
-    coin = int(input())
-    coin_list.append(coin)
+    coin_list.append(int(input()))
 
+# 동전 내림차순 정렬
 coin_list.sort(reverse=True)
 
+# 동전의 개수 셀 변수 
 count = 0
+
+# 동전 개수 구하기
 for coin in coin_list:
     if k==0: break
-    val = k//coin
-    count += val
-    k = k%coin
-print(count)
-print(coin_list)
+    val = k//coin # 몫 = 동전의 개수 
+    count += val # 동전의 개수 더해주기
+    k = k%coin # 나머지 값 k에 저장
+print(count) # 필요한 동전의 최소 개수 
