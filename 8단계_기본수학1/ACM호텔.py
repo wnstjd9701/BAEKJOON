a0eae5c9c1d6b02552eeeefbc44d168f66a1468f
wnstjd9701 <wnstjd9701@naver.com>
2021-09-01 01:54:38 +0900

    BEAKJOON

---
+++ 8단계_기본수학1/ACM호텔.py
@@ -0,0 +1,14 @@
+# 백준 10250번 문제 - ACM호텔
+test_data = int(input())
+for i in range(test_data):
+    h, w, n = map(int, input().split())
+    f = 0 
+    hotel = 0 
+    if n % h ==0:
+        f = h*100
+        hotel = n // h
+    else:
+        f = (n % h) * 100
+        hotel = 1 + n // h
+    print(f+hotel)
+    
