eab313fd8a2eba424c53d94dd29c45e1441658d3
wnstjd9701 <wnstjd9701@naver.com>
2021-09-04 20:44:56 +0900

    2021-09-04

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-def recursive_function(i):
-    if i == 100:
-        return 
-    print(i, '번째 재귀 함수에서', i+1, '번째 재귀 함수를 호출합니다.')
-    recursive_function(i+1)
-    print(i, '번째 재귀 함수를 종료합니다.')
+def factorial_function(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
 
-recursive_function(1)
+def factorial_recursive(n):
+    if n <= 1:
+        return 1
+    return n * factorial_recursive(n-1)
