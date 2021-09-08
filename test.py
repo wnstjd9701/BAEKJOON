4f98456af3c8a22c5df3290fe28120ad5db7429d
wnstjd9701 <wnstjd9701@naver.com>
2021-09-08 20:53:01 +0900

    Stack

--- test.py
+++ test.py
@@ -1,23 +1,5 @@
-def dfs(graph, v, visited):
-    visited[v] = True
-    print(v, end=" ")
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
-graph = [
-    [],
-    [2,3,8],
-    [1,7],
-    [1,4,5],
-    [3,5],
-    [3,4],
-    [7],
-    [2,6,8],
-    [1,7]
-]
+import sys
+for i in range(2):
+    n = sys.stdin.readline().split()
 
-visited = [False] * 9
-print(graph[1])
-print(visited)
-
-dfs(graph, 1, visited)
+print(n[0][0], " ", n[1])
