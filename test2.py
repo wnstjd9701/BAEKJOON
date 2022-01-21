0c7b29e54949bf95f19ef4c8e27f497b56376e4e
Yun Jun Sung <wnstjd9701@naver.com>
2022-01-21 17:28:05 +0900

    2021-01-21

--- test2.py
+++ test2.py
@@ -1,9 +1,10 @@
-INF = 99999999
+graph  =[[] for _ in range(3)]
 
-graph = [
-    [0,7,5],
-    [7,0,INF],
-    [5,INF,0]
-]
-print(graph[0][1])
-# DFS
+graph[0].append((1,7))
+graph[0].append((2,5))
+
+graph[1].append((0,7))
+
+graph[2].append((0,5))
+
+print(graph)
