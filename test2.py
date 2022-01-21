ba6991dc7a2fb1ae8e8a51b3907246edc556cc8c
Yun Jun Sung <wnstjd9701@naver.com>
2022-01-21 18:13:48 +0900

    5-10.py

--- test2.py
+++ test2.py
@@ -1,10 +1,26 @@
-graph  =[[] for _ in range(3)]
+from collections import deque
 
-graph[0].append((1,7))
-graph[0].append((2,5))
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
 
-graph[1].append((0,7))
+graph = [[],
+[2,3,8],
+[1,7],
+[1,4,5],
+[3,5],
+[3,4],
+[7],
+[2,6,8],
+[1,7]
+]
 
-graph[2].append((0,5))
-
-print(graph)
+visited = [False] * 9
+bfs(graph, 1, visited)
