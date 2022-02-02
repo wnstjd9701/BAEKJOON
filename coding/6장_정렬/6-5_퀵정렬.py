50c60c3fd3b8042edc4e1a55ac45917957be8182
Yun Jun Sung <wnstjd9701@naver.com>
2022-02-02 11:01:09 +0900

    2022-02-02

--- coding/6장_정렬/6-5_퀵정렬.py
+++ coding/6장_정렬/6-5_퀵정렬.py
@@ -6,7 +6,7 @@ def quick_sort(array):
         return array
     
     pivot = array[0] # 피벗은 첫 번째 원소
-    tail = array[1::] # 피벗을 제외한 리스트
+    tail = array[1:] # 피벗을 제외한 리스트
 
     left_side = [x for x in tail if x <= pivot] # 분할되 왼쪽 부분
     right_side = [x for x in tail if x > pivot] # 분할된 오른쪽 부분
