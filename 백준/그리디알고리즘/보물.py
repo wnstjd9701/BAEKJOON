ab3088037e6812b3e5beb05ea7cba3e549e74557
Yun Jun Sung <wnstjd9701@naver.com>
2022-02-17 15:46:24 +0900

    2022-02-17

--- 백준/그리디알고리즘/보물.py
+++ 백준/그리디알고리즘/보물.py
@@ -1,7 +1,4 @@
 # 백준 1026번 문제 - 보물
-from os import sep
-
-
 n = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
@@ -11,5 +8,3 @@ S = 0
 for i in range(n):
     S += a[i]*b[i]
 print(S)
-print(a, sep="")
-print(b)
