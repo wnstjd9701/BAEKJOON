a7e1cc51fd3ba1b6b4b9c9b78edf8354d1e00d33
wnstjd9701 <wnstjd9701@naver.com>
2022-03-07 02:32:23 +0900

    2022-03-07

---
+++ 프로그래머스/1단계/2016.py
@@ -0,0 +1,15 @@
+# 프로그래머스 1단계 - 2016
+def solution(a, b):
+    date = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    answer = date[(sum(month[:a-1])+b-1)%7] 
+    print(type(answer))
+    return answer
+import datetime
+
+def getDayName(a,b):
+    t = 'MON TUE WED THU FRI SAT SUN'.split()
+    return t[datetime.datetime(2016, a, b).weekday()]
+
+
+
