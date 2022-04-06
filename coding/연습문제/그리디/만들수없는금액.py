65599a965ec7a261bdbf005f42da455e2e26bab7
wnstjd9701 <wnstjd9701@naver.com>
2022-04-06 22:27:54 +0900

    2022-04-06

---
+++ coding/연습문제/그리디/만들수없는금액.py
@@ -0,0 +1,14 @@
+# 연습문제 - 그리디 - 만들 수 없는 금액
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+target = 1
+for x in data:
+    # 만들 수 없는 금액을 찾았을 떄 반복 종료
+    if target < x:
+        break
+    target += x
+
+# 만들 수 없는 금액 출력
+print(target)
