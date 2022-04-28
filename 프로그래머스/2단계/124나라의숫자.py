134b433df01f72261ca92d13a7801cc4d3b7f736
wnstjd9701 <wnstjd9701@naver.com>
2022-04-28 15:45:40 +0900

    2022-04-28

---
+++ 프로그래머스/2단계/124나라의숫자.py
@@ -0,0 +1,11 @@
+# 프로그래머스 2단계 - 124 나라의 숫자
+def solution(n):
+    answer = ''
+    while n:
+        if n % 3:
+            answer += str(n%3)
+            n //= 3
+        else:
+            answer += "4"
+            n = n//3 - 1
+    return answer[::-1]
