740bffa43e49bb5154b0be4c81fa1d085f3e5039
Yun Jun Sung <wnstjd9701@naver.com>
2022-10-06 22:36:43 +0900

    프로그래머스 2단계

---
+++ 프로그래머스/2단계/올바른괄호.py
@@ -0,0 +1,12 @@
+# 프로그래머스 2단계 - 스택/큐 - 올바른 괄호
+def solution(s):
+    answer = True
+    stack = []
+    for t in s:
+        if t == '(':
+            stack.append(t)
+        elif len(stack) and t == ')':
+            stack.pop()
+        else:
+            return False
+    return False if len(stack) else True
