e2dcb92d6c9c95d9cb2cefe6d8640fcb4205579f
wnstjd9701 <wnstjd9701@naver.com>
2022-10-11 21:04:12 +0900

    다이나믹 프로그래밍

--- 백준/백트래킹/N과M2.py
+++ 백준/백트래킹/N과M2.py
@@ -1,4 +1,28 @@
 # 백준 15650번 문제 - N 과 M (2)
+N, M = map(int, input().split())
+visited = [0 for _ in range(N)]
+arr = []
+
+def dfs(cnt):
+    if cnt == M:
+        print(*arr)
+        return
+
+    for i in range(N):
+        if visited[i] == 0:
+            visited[i] = 1  # 중복 제거
+            arr.append(i+1)
+
+            dfs(cnt+1)  # 다음 깊이 탐색
+            arr.pop()  # 탐사한 내용 제거
+
+            # 순열 부분과의 차이점
+            for j in range(i+1, N):
+                visited[j] = 0
+
+dfs(0)
+
+
 from itertools import combinations
 
 N, M = map(int, input().split())
