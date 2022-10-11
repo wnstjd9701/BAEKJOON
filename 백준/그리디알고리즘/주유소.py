c983760660befbed51b399f327788da6fc35430a
wnstjd9701 <wnstjd9701@naver.com>
2022-10-11 19:25:53 +0900

    그리디

--- 백준/그리디알고리즘/주유소.py
+++ 백준/그리디알고리즘/주유소.py
@@ -1,25 +1,5 @@
 # 백준 13305번 문제 - 주유소
 n = int(input())
-distance = list(map(int, input().split()))
-costs = list(map(int, input().split()))
-
-res = distance[0] * costs[0]
-m = costs[0]
-dist = 0
-for i in range(1, n-1):
-    if costs[i] < m: 
-        res += m * dist
-        dist = distance[i]
-        m = costs[i]
-    else:
-        dist += distance[i]
-
-    if i == n-1:
-        res += m * dist
-        
-print(res)
-
-n = int(input())
 roads = list(map(int, input().split()))
 costs = list(map(int, input().split()))
 
