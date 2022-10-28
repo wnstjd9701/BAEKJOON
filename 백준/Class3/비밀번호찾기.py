9286a5580fc2de83d939b6485cae1b7f9b84aa51
wnstjd9701 <wnstjd9701@naver.com>
2022-10-28 22:40:39 +0900

    Class3

--- 백준/Class3/비밀번호찾기.py
+++ 백준/Class3/비밀번호찾기.py
@@ -1,1 +1,11 @@
 # 백준 17219번 문제 - 비밀번호 찾기
+import sys
+n, m = map(int, sys.stdin.readline().strip().split())
+dict = {}
+
+for _ in range(n):
+    key, value = sys.stdin.readline().strip().split()
+    dict[key] = value 
+for _ in range(m):
+    print(dict[sys.stdin.readline().rstrip()])
+
--- 백준/별찍기-3.py
+++
@@ -1,9 +0,0 @@
-# 백준 2440번 문제 - 별 찍기-3
-from os import sep
-
-
-n = int(input())
-for i in range(n, 0, -1):
-    print('*' * i)
-
-# end..
