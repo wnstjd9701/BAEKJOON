66b550aac438bf5db4704d7f5a18fe3c827c877d
Yun Jun Sung <wnstjd9701@naver.com>
2022-10-31 05:23:28 +0900

    프로그래머스 스킬테스트

--- 프로그래머스/스킬체크테스트Level2/1.py
+++ 프로그래머스/스킬체크테스트Level2/1.py
@@ -1,4 +1,12 @@
 # 스킬 체크 테스트 Level2 - 1번 문제
-arr1 = [[1, 4], [3, 2], [4, 1]]
-arr2 = [[3, 3], [3, 3]]
-print(arr1*arr2)
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        temp_list = []
+        for j in range(len(arr2[0])):
+            temp = 0
+            for k in range(len(arr2)):
+                temp += arr1[i][k] * arr2[k][j]
+            temp_list.append(temp)
+        answer.append(temp_list)
+    return answer
