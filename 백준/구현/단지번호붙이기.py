7362be1834288b286ecbf103e7e756994d3172af
Yun Jun Sung <wnstjd9701@naver.com>
2022-10-31 05:23:12 +0900

    구현

---
+++ 백준/구현/단지번호붙이기.py
@@ -0,0 +1,84 @@
+# 백준 2667번 문제 - 단지번호붙이기
+n = int(input())
+graph = []
+num = []
+
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+def DFS(x,y):
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return False
+    
+    if graph[x][y] == 1:
+        global cnt
+        cnt += 1
+        graph[x][y] = 0
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            DFS(nx, ny)
+        return True
+    return False
+
+cnt = 0 
+result = 0
+
+for i in range(n):
+    for j in range(n):
+        if DFS(i, j) == True:
+            num.append(cnt)
+            result += 1
+            cnt = 0
+
+num.sort()
+print(result)
+for i in num:
+    print(i)
+
+# BFS
+from collections import deque
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+
+def bfs(graph, a, b):
+    n = len(graph)
+    queue = deque()
+    queue.append((a, b))
+    graph[a][b] = 0
+    count = 1
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append((nx, ny))
+                count += 1
+    return count
+
+
+n = int(input())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+cnt = []
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            cnt.append(bfs(graph, i, j))
+
+cnt.sort()
+print(len(cnt))
+for i in range(len(cnt)):
+    print(cnt[i])
