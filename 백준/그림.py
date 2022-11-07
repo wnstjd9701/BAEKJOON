aa62fee828170d1dc3b45093e524d6d6283a0d84
Yun Jun Sung <wnstjd9701@naver.com>
2022-11-07 19:58:40 +0900

    백준

--- 백준/그림.py
+++ 백준/그림.py
@@ -1,21 +1,21 @@
+# 백준 1926번 문제 - 그림
 from collections import deque
 n, m = map(int, input().split())
-for i in range(n):
-    graph = [list(map(int, input().split()))]
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
 
 dx = [0,0,-1,1]
 dy = [1,-1,0,0]
 
-paint = []
 def bfs(graph, x, y):
     queue = deque()
     queue.append((x,y))
     graph[x][y] = 0
-    count = 1 # 면적을 계산할 변수
+    count = 1
 
     while queue:
         x, y = queue.popleft()
-        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -23,11 +23,11 @@ def bfs(graph, x, y):
                 continue
             if graph[nx][ny] == 1:
                 graph[nx][ny] = 0
-                bfs(graph, nx, ny)
                 queue.append((nx, ny))
                 count += 1
     return count
 
+paint = []
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 1:
