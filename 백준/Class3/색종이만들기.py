b65e31d1d96b838405d31bd41034bc89f6ce0128
Yun Jun Sung <wnstjd9701@naver.com>
2022-11-09 06:35:45 +0900

    Class3

--- 백준/Class3/색종이만들기.py
+++ 백준/Class3/색종이만들기.py
@@ -1,27 +1,26 @@
 # 백준 2630번 문제 - 색종이 만들기
-import sys
-
-N = int(sys.stdin.readline())
-paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)] 
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
 result = []
 
-def solution(x, y, N) :
-  color = paper[x][y]
-  for i in range(x, x+N) :
-    for j in range(y, y+N) :
-      if color != paper[i][j] :
-        solution(x, y, N//2)
-        solution(x, y+N//2, N//2)
-        solution(x+N//2, y, N//2)
-        solution(x+N//2, y+N//2, N//2)
-        return
-  if color == 0 :
-    result.append(0)
-  else :
-    result.append(1)
+def solution(x, y, n):
+    half = n // 2
+    color = graph[x][y]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if color != graph[i][j]: # 하나라도 다르다면
+                solution(x,y,half)
+                solution(x,y+half,half)
+                solution(x+half,y,half)
+                solution(x+half,y+half,half)
+                return
+    # 모두 같은 색인 경우
+    if color == 0:
+        result.append(0)
+    else:
+        result.append(1)
 
-
-solution(0,0,N)
+solution(0,0,n)
 print(result.count(0))
 print(result.count(1))
