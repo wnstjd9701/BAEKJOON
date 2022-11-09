c7dd5b914fb071b8cb2889a73624877442fd3903
Yun Jun Sung <wnstjd9701@naver.com>
2022-11-10 04:18:16 +0900

    프로그래머스 3단계

---
+++ 프로그래머스/2단계/단어변환.py
@@ -0,0 +1,24 @@
+# 프로그래머스 2단계 - DFS/BFS 단어 변환
+from collections import deque
+
+def solution(begin, target, words):
+    answer = 0
+    q = deque()
+    q.append([begin,0])
+    v = [0 for _ in range(len(words))]
+    while q:
+        word, cnt = q.popleft()
+        if word == target:
+            answer = cnt
+            break
+        for i in range(len(words)):
+            temp_cnt = 0
+            if not v[i]:
+                for j in range(len(word)):
+                    if word[j] != words[i][j]:
+                        temp_cnt += 1
+                if temp_cnt == 1:
+                    q.append([words[i], cnt+1])
+                    v[i] = 1
+    return answer
+print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
