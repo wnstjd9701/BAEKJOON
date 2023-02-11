7ae86714db78c8f8def7ce1801a824ab4fe81ca0
Yun Jun Sung <wnstjd9701@naver.com>
2023-02-11 23:18:46 +0900

    Problem Solving

---
+++ 백준/1_문제순위/동전2.py
@@ -0,0 +1,14 @@
+# 백준 2294번 문제 - 동전 2
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+dp = [10001] * (k+1)
+dp[0] = 0
+
+for coin in coins:
+    for i in range(coin, k+1):
+        dp[i] = min(dp[i], dp[i-coin]+1)
+# print(dp)
+if dp[k] == 10001:
+    print(-1)
+else:
+    print(dp[k])
