e00ee1522c9e777f7f8be5b18c2108c835c7ef60
Yun Jun Sung <wnstjd9701@naver.com>
2023-03-28 00:19:11 +0900

    Problem Solving

--- 백준/1_문제순위/카드정렬하기.py
+++ 백준/1_문제순위/카드정렬하기.py
@@ -1,1 +1,21 @@
 # 백준 1715번 문제 - 카드 정렬하기
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cards = []
+result = 0
+
+for i in range(n):
+    heapq.heappush(cards, int(input()))
+
+if len(cards) == 1:
+    print(0)
+else:
+    while len(cards) > 1:
+        plus = heapq.heappop(cards) + heapq.heappop(cards)
+        result += plus
+        heapq.heappush(cards, plus)
+    
+    print(result)
