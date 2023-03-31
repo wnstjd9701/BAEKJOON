bd67df4ffc399b57c3ae95d41368d4cbaee55afb
wnstjd9701 <wnstjd9701@naver.com>
2023-03-31 16:24:43 +0900

    Problem Solving

---
+++ 백준/1_문제순위/가장긴감소하는부분수열.py
@@ -0,0 +1,12 @@
+# 백준 11722번 문제 - 가장 긴 감소하는 부분수열
+n = int(input())
+arr = list(map(int, input().split()))
+
+dp = [1] * n
+for i in range(n):
+    for j in range(1, i):
+        if arr[i] < arr[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+print(max(dp))
+
+print(dp)
