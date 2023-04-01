f415d0b9e4a7520351d1f2d54a28d98e030cde6b
Yun Jun Sung <wnstjd9701@naver.com>
2023-04-01 17:38:48 +0900

    Problem Solving

--- CODETREE/수열조작.py
+++
@@ -1,9 +0,0 @@
-# 수열 조작 - Deque
-from collections import deque
-
-n = int(input())
-queue = deque()
-for i in range(1, n+1):
-    queue.append(i)
-
-print(queue)
--- test2.py
+++
@@ -1,8 +0,0 @@
-from asyncio.windows_events import NULL
-
-
-team = [[] for _ in range(6)]
-if len(team[0]) == 0:
-    print(0)
-print(team)
-# test
