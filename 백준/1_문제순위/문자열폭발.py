2d30240f88ce5af5a204634123e59a127b53a77e
wnstjd9701 <wnstjd9701@naver.com>
2023-04-03 11:40:34 +0900

    Problem Solving

---
+++ 백준/1_문제순위/문자열폭발.py
@@ -0,0 +1,22 @@
+# 백준 9935번 문제 - 문자열 폭발
+import sys
+input = sys.stdin.readline
+# 입력값 처리
+s = input().rstrip()
+bomb = input().rstrip()
+
+# stack으로 문자열 폭발 구현
+stack = []
+bomb_length = len(bomb)
+
+for char in s:
+    stack.append(char)
+    if ''.join(stack[-bomb_length:]) == bomb:
+        for _ in range(bomb_length):
+            stack.pop()
+
+# 결과 출력
+if stack:
+    print(''.join(stack))
+else:
+    print('FRULA')
