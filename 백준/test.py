668338d08137ba34772488f5046a4640789fc6e2
wnstjd9701 <wnstjd9701@naver.com>
2023-04-03 17:44:40 +0900

    Problem Solving

---
+++ 백준/test.py
@@ -0,0 +1,27 @@
+# test python
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0]*n]
+min_diff = int(1e9)
+
+def dfs(depth, idx):
+    global min_diff
+    if depth == n // 2:
+        power1, power2 = 0, 0
+        for i in range(n):
+            for j in range(n):
+                if visited[i] and visited[j]:
+                    power1 += graph[i][j]
+                elif not visited[i] and not visited[j]:
+                    power2 += graph[i][j]
+        min_diff = min(min_diff, abs(power1 - power2))
+        return
+
+    for i in range(idx, n):
+        if not visited[i]:
+            visited[i] = 1
+            dfs(depth+1, i+1)
+            visited[i] = 0
+
+dfs(0,0)
+print(min_diff)
