62b570826b688374c2526e0f10e910a623550fb7
wnstjd9701 <wnstjd9701@naver.com>
2023-05-08 15:23:14 +0900

    Problem Solving

---
+++ 백준/1_문제순위/테트로미노.py
@@ -0,0 +1,40 @@
+# 백준 14500번 문제 - 테트로미노
+import sys
+input = sys.stdin.readline
+
+def dfs(r, c, idx, total):
+    global ans
+    if ans >= total + max_val * (3 - idx):
+        return
+    if idx == 3:
+        ans = max(ans, total)
+        return
+    else:
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < n and 0 <= nc < m and visit[nr][nc] == 0:
+                if idx == 1:
+                    visit[nr][nc] = 1
+                    dfs(r, c, idx + 1, total + arr[nr][nc])
+                    visit[nr][nc] = 0
+                visit[nr][nc] = 1
+                dfs(nr, nc, idx + 1, total + arr[nr][nc])
+                visit[nr][nc] = 0
+
+
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+visit = [([0] * m) for _ in range(n)]
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+ans = 0
+max_val = max(map(max, arr))
+
+for r in range(n):
+    for c in range(m):
+        visit[r][c] = 1
+        dfs(r, c, 0, arr[r][c])
+        visit[r][c] = 0
+
+print(ans)
