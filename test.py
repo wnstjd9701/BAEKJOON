101787b4dd55d171c72b22cad79d85342d10d450
wnstjd9701 <wnstjd9701@naver.com>
2024-03-28 14:18:20 +0900

    240328

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-# Test
-def print_fibonacci_up_to_n():
-    a, b, n = map(int, input().split())
-    while a <= n:
-        print(a, end=" ")
-        a, b = b, a + b
-
-# 40 이하까지의 피보나치 수 출력
-print_fibonacci_up_to_n()
+N = int(input())
+pwd = list(input())
+stack = []
+for i in range(len(pwd)):
+    if pwd[i] == '#':
+        if len(stack) == 0:
+            continue
+        stack.pop()
+    else:
+        stack.append(pwd[i])
+print(stack)
